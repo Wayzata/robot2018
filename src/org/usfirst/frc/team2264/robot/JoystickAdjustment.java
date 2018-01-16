ce2c43d9f86022a2449938b37e9fd22166b307f8
marcelnerd <31940874+marcelnerd@users.noreply.github.com>
2018-01-16 16:48:29 -0600

    Joysticks
    Class and methods for getting readings from the joysticks, including the proper exponential adjustments and human-error deadzones

---
+++ src/org/usfirst/frc/team2264/robot/JoystickAdjustment.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team2264.robot;
+// @author Cameron
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class JoystickAdjustment {
+
+	static double sensitivityAdjustment(double position) {
+		double adjustment = Math.pow(position, 2);
+		
+		if(position >= 0) {
+			return adjustment;
+		}
+		else {
+			return -1 * adjustment;
+		}
+	}
+	
+	static double getLeft(Joystick leftJoystick, Joystick rightJoystick) {
+		
+		if(rightJoystick.getY(GenericHID.Hand.kRight) - leftJoystick.getY(GenericHID.Hand.kLeft) <= 0.10) {
+			
+			SmartDashboard.putNumber("Left Joystick: ", rightJoystick.getY(GenericHID.Hand.kRight));
+			return rightJoystick.getY(GenericHID.Hand.kRight);
+		}
+		else {
+			SmartDashboard.putNumber("Left Joystick: ", leftJoystick.getY(GenericHID.Hand.kLeft));
+			return leftJoystick.getY(GenericHID.Hand.kLeft);
+		}
+	}
+	
+	static double getRight(Joystick rightJoystick) {
+		SmartDashboard.putNumber("Right Joystick: ", rightJoystick.getY(GenericHID.Hand.kRight));
+		return rightJoystick.getY(GenericHID.Hand.kRight);
+	}
+}
