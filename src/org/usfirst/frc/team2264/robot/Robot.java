d193e48293d7cbedbcaa6894622594b6802b0d71
DrakeJohnson <johnsdra000@isd284.com>
2018-01-29 18:20:07 -0600

    Add files via upload

--- src/org/usfirst/frc/team2264/robot/Robot.java
+++ src/org/usfirst/frc/team2264/robot/Robot.java
@@ -1,14 +1,17 @@
 package org.usfirst.frc.team2264.robot;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// Master Code
 
 // @Authors
 /* Drake, Lexi, Rafael, Cameron, Nathan, Julian, Preeti */
@@ -17,15 +20,35 @@ public class Robot extends IterativeRobot {
 	
 // Main class that will use other classes and call methods to run the robot
 	
+	//autonomous+ smart dashboard
+	final String driveAuto = "drive straight";
+	final String centerAuto = "Center Auto";
+	final String leftAuto= "Left Auto";
+	final String rightAuto= "Right Auto";
+	final String noAuto= "no Auto";
+	String autoSelected;
+	String gameData= DriverStation.getInstance().getGameSpecificMessage();
+	long autoStartTime;
+	long timeInAuto;
+	SendableChooser<String> chooser = new SendableChooser<>();
+	Autos auto;
+	
 	//Joysticks
 	Joystick leftJ;
 	Joystick rightJ;
+	XboxController controller;
 	
 	//Motors/Talons
 	TalonSRX frontLeft;
 	TalonSRX frontRight;
 	TalonSRX backLeft;
 	TalonSRX backRight;
+	TalonSRX intakeLeft;
+	TalonSRX intakeRight;
+	TalonSRX conveyorLeft;
+	TalonSRX conveyorRight;
+	TalonSRX shooterLeft;
+	TalonSRX shooterRight;
 	
 	//Gyro
 	ADXRS450_Gyro Gyro;
@@ -33,10 +56,22 @@ public class Robot extends IterativeRobot {
 	double gyroTrack;
 	boolean gyroPluggedIn;
 	
+	/* Cam stuff
+	Shooter shootyBoi = new Shooter();
+	Intake grabbyBoi = new Intake();
+	Conveyor movyBoi = new Conveyor();
+	*/
+	
 	public void robotInit() {
 		
 		// Method that will run only one time in Teleop
-		
+		// Smart dashboard
+		chooser.addObject("Center Auto", centerAuto);
+		chooser.addObject("Left Auto", leftAuto);
+		chooser.addObject("Right Auto", rightAuto);
+		chooser.addObject("Drive straight", driveAuto);
+		chooser.addDefault("no auto", noAuto);
+		SmartDashboard.putData("Auto choices", chooser);
 		// Motors
 		frontLeft = new TalonSRX(Variables.frontLN);
 		frontRight = new TalonSRX(Variables.frontRN);
@@ -46,39 +81,112 @@ public class Robot extends IterativeRobot {
 		// Joysticks
 		leftJ = new Joystick(Variables.leftStickPort);
 		rightJ = new Joystick(Variables.rightStickPort);
+		controller = new XboxController(Variables.controllerPort);
 		
 		// Gyro
 		Gyro = new ADXRS450_Gyro();
-		
 	}
 	
-	public void autoInit() {
-		
+	public void autonomousInit() {
 		// Method that will run only one time in autonomous
-		
 		Gyro.calibrate();
 		gyroPluggedIn = Util.gyroCheck(Gyro);
+		this.autoStartTime = System.currentTimeMillis();
+		autoSelected = chooser.getSelected();
+		System.out.println("Auto selected: " + autoSelected);
+	}
+
+
+	@Override
+	public void autonomousPeriodic() {
+
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	
+		timeInAuto=System.currentTimeMillis()- autoStartTime;
+		switch (autoSelected) {
+		case leftAuto:
+			if(auto.getSwitch(gameData, 0)){
+			auto.leftLeft(frontLeft,frontRight, backLeft, backRight, Gyro, timeInAuto);
+			}
+			else{
+				auto.leftRight(frontLeft,frontRight, backLeft, backRight, Gyro, timeInAuto);
+			}
+			break;
+		case rightAuto:
+			if(auto.getSwitch(gameData, 1)){
+			auto.rightRight(frontLeft,frontRight, backLeft, backRight, Gyro, timeInAuto);
+			}
+			else{
+				auto.rightLeft(frontLeft,frontRight, backLeft, backRight, Gyro, timeInAuto);
+			}
+		case centerAuto:
+			//auto.center(left, right);
+			break;
+		//case straightSwitch:
+			//side=1;
+		//	auto.sideChoice(left, right, side);
+		case driveAuto:
+			auto.driveForward(frontLeft, frontRight, backLeft, backRight, Gyro, 0);
+			break;
+		default:
+			auto.stop(frontLeft,frontRight, backLeft, backRight);
+			break;
+		}
+		
 	}
 	
-	public void robotPeriodic() {
+
+	/* Cameron's shooterboi, grabberboi, movyboi classes
+	void checkButtons() {
+		if(controller.getAButtonPressed()) {
+			shooter.startShooter(shooterLeft, shooterRight);
+		}
+		if(controller.getBButtonPressed()) {
+			shooter.stopShooter(shooterLeft, shooterRight);
+		}
+		if(controller.getXButtonPressed()) {
+			grabber.startIntake(intakeLeft, intakeRight);
+		}
+		if(controller.getYButtonPressed()) {
+			grabber.stopIntake(intakeLeft, intakeRight);
+		}
+		if(controller.getBumperPressed(GenericHID.Hand.kLeft)) {
+			mover.startConveyor(conveyorLeft, conveyorRight);
+		}
+		if(controller.getBumperPressed(GenericHID.Hand.kRight)) {
+			mover.stopConveyor(conveyorLeft, conveyorRight);
+		}
+	}
+	*/
+	
+
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+
 		
 		// Method that will be constantly called during Teleop
 		
+		//checkButtons();
+		
 		SmartDashboard.putNumber("Gyro Value: ", Gyro.getAngle());
-	
 		DriveTrain.MotorSet(leftJ, rightJ, frontLeft, frontRight, backLeft, backRight);
 	
-	}
-	
-	public void autoPeriodic() {
-			
-		// Method that will be constantly called during autonomous
+		/* 4 motors vs 2 motors (Cameron)
+		if(Variables.whichRobot == RobotChoice.MARS) {
+		DriveTrain.MotorSet(leftJ, rightJ, frontLeft, frontRight, backLeft, backRight);
+		}
+		else {
+			DriveTrain.MotorSetTwo(leftJ, rightJ, backLeft, backRight);
+		}
+		*/
 		
-		SmartDashboard.putNumber("Gyro Value: ", Gyro.getAngle());
-		
-		// Cases for each auto option
-			//Calls a mehtod from the auto class based on case
-		
-	}
-	
+	}	
 }
+
--- src/org/usfirst/frc/team2264/robot/Variables.java
+++ src/org/usfirst/frc/team2264/robot/Variables.java
@@ -4,47 +4,32 @@ package org.usfirst.frc.team2264.robot;
 /* Drake, Lexi, Rafael, Cameron, Nathan, Julian, Preeti */
 
 public class Variables {
-	public static RobotChoice whichRobot = RobotChoice.MARS;
+	
 	// Variables class to change numbers and use them throughout the code
 	
 	// Motors
-	
 	public static final int frontLN = 4;
 	public static final int frontRN = 3;
-	public static int backLN = 6;
-	public static int backRN = 5;
-
-	public static final double intakeSpeed = .5; ///SET THIS NUMBER///
-	public static final double conveyorSpeed = .5; //SET THIS NUMBER///
-	public static final double shooterSpeed = .5; //SET THIS NUMBER///
-
-
-	
+	public static final int backLN = 6;
+	public static final int backRN = 5;
 	
 	// Joysticks
 	public static final int rightStickPort = 1;
 	public static final int leftStickPort = 0;
 	
-	//Gamepad
-	public static final int controllerPort = 9999; ///SET THIS NUMBER///
+	//Gamepad 
+	public static final int controllerPort = 2;
 	// 2 shooting
 	// 2 moving
 	// 2 grabbing
 	
-	public Variables() {
-		if(whichRobot == RobotChoice.MARS) {
-			backLN = 6;
-			backRN = 5;
-		}
-		else if(whichRobot == RobotChoice.VERONICA) {
-			backLN = 1;
-			backRN = 2;
-		}
-		else {
-			backLN = 25;
-			backRN = 26;
-		} 
-	}
+	// Robot
+	// 1 is Mars
+	public static final int whichRobot = 1;
 	
-}
+	
+	
+	
+	
 
+}
